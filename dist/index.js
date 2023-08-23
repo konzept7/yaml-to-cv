cc00860d7538ebe8bdf22f28248d9383a049ac87
Frank Bielecke <frank.bielecke@gmail.com>
2023-08-24 01:20:36 +0200

    english cvs

--- dist/index.js
+++ dist/index.js
@@ -8,38 +8,54 @@ const fs_1 = require("fs");
 const handlebars_1 = __importDefault(require("handlebars"));
 const marked_1 = __importDefault(require("marked"));
 // read handlebars template
-const templateFile = (0, fs_1.readFileSync)('./src/template.hbs', 'utf8');
+const templateFile = (0, fs_1.readFileSync)("./src/template.hbs", "utf8");
+const englishTemplateFile = (0, fs_1.readFileSync)("./src/template.en.hbs", "utf8");
 const template = handlebars_1.default.compile(templateFile);
+const englishTemplate = handlebars_1.default.compile(englishTemplateFile);
 // read all files in data directory
-const files = (0, fs_1.readdirSync)('./data');
+const files = (0, fs_1.readdirSync)("./data");
 // loop through each file
-files.forEach(file => {
+files.forEach((file) => {
     // copy jpg file to output directory
-    if (file.endsWith('.jpg') || file.endsWith('.jpeg')) {
+    if (file.endsWith(".jpg") || file.endsWith(".jpeg")) {
         (0, fs_1.copyFileSync)(`./data/${file}`, `./output/${file}`);
         return;
     }
     // make sure we only process yaml files
-    if (!file.endsWith('.yaml')) {
+    if (!file.endsWith(".yaml")) {
         return;
     }
-    const fileContents = (0, fs_1.readFileSync)(`./data/${file}`, 'utf8');
+    const isEnglish = file.endsWith("en.yaml");
+    const fileContents = (0, fs_1.readFileSync)(`./data/${file}`, "utf8");
     const yaml = (0, yaml_1.parse)(fileContents, {});
-    handlebars_1.default.registerHelper('repeat', function (times, options) {
+    handlebars_1.default.registerHelper("repeat", function (times, options) {
         // handlebars safe string to prevent html escaping
         return new handlebars_1.default.SafeString(options.fn(this).repeat(times));
     });
-    handlebars_1.default.registerHelper('markdown', function (text) {
+    handlebars_1.default.registerHelper("markdown", function (text) {
         return new handlebars_1.default.SafeString(marked_1.default.parse(text));
     });
-    handlebars_1.default.registerHelper('add', function (a, b) {
+    handlebars_1.default.registerHelper("add", function (a, b) {
         return a + b;
     });
-    handlebars_1.default.registerHelper('subtract', function (a, b) {
+    handlebars_1.default.registerHelper("subtract", function (a, b) {
         return a - b;
     });
-    const hasPhoto = (0, fs_1.existsSync)(`./data/${file.replace('.yaml', '.jpg')}`);
-    const html = template(Object.assign(Object.assign({}, yaml), { file: hasPhoto ? file.replace('.yaml', '.jpg') : null }));
-    (0, fs_1.writeFileSync)(`./output/${file.replace('.yaml', '.html')}`, html);
+    handlebars_1.default.registerHelper("toLocaleDate", function (locale, text) {
+        const date = new Date(text);
+        return date.toLocaleDateString(locale, {
+            year: "numeric",
+            month: "numeric",
+            day: "numeric",
+        });
+    });
+    const photoLocation = file
+        .replace(".en.yaml", ".jpg")
+        .replace(".yaml", ".jpg");
+    const hasPhoto = (0, fs_1.existsSync)(`./data/${photoLocation}`);
+    const html = isEnglish
+        ? englishTemplate(Object.assign(Object.assign({}, yaml), { file: hasPhoto ? photoLocation : null }))
+        : template(Object.assign(Object.assign({}, yaml), { file: hasPhoto ? photoLocation : null }));
+    (0, fs_1.writeFileSync)(`./output/${file.replace(".yaml", ".html")}`, html);
 });
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,48 +1,75 @@
-import { parse } from 'yaml'
-import { readFileSync, readdirSync, writeFileSync, copyFileSync, existsSync } from 'fs'
-import Handlebars from 'handlebars';
-import marked from 'marked';
-
+import { parse } from "yaml";
+import {
+  readFileSync,
+  readdirSync,
+  writeFileSync,
+  copyFileSync,
+  existsSync,
+} from "fs";
+import Handlebars from "handlebars";
+import marked from "marked";
 
 // read handlebars template
-const templateFile = readFileSync('./src/template.hbs', 'utf8')
-const template = Handlebars.compile(templateFile)
-
+const templateFile = readFileSync("./src/template.hbs", "utf8");
+const englishTemplateFile = readFileSync("./src/template.en.hbs", "utf8");
+const template = Handlebars.compile(templateFile);
+const englishTemplate = Handlebars.compile(englishTemplateFile);
 // read all files in data directory
-const files = readdirSync('./data');
+const files = readdirSync("./data");
 // loop through each file
-files.forEach(file => {
-
+files.forEach((file) => {
   // copy jpg file to output directory
-  if (file.endsWith('.jpg') || file.endsWith('.jpeg')) {
-    copyFileSync(`./data/${file}`, `./output/${file}`)
-    return
+  if (file.endsWith(".jpg") || file.endsWith(".jpeg")) {
+    copyFileSync(`./data/${file}`, `./output/${file}`);
+    return;
   }
 
   // make sure we only process yaml files
-  if (!file.endsWith('.yaml')) {
-    return
+  if (!file.endsWith(".yaml")) {
+    return;
   }
+  const isEnglish = file.endsWith("en.yaml");
 
-  const fileContents = readFileSync(`./data/${file}`, 'utf8')
-  const yaml = parse(fileContents, {})
+  const fileContents = readFileSync(`./data/${file}`, "utf8");
+  const yaml = parse(fileContents, {});
 
-    Handlebars.registerHelper('repeat', function (times: number, options) {
-      // handlebars safe string to prevent html escaping
-    return new Handlebars.SafeString(options.fn(this).repeat(times))
-  })
-  Handlebars.registerHelper('markdown', function (text: string) {
-    return new Handlebars.SafeString(marked.parse(text))
-  })
-  Handlebars.registerHelper('add', function (a: number, b: number) {
-    return a + b
-  })
-  Handlebars.registerHelper('subtract', function (a: number, b: number) {
-    return a - b
-  })
+  Handlebars.registerHelper("repeat", function (times: number, options) {
+    // handlebars safe string to prevent html escaping
+    return new Handlebars.SafeString(options.fn(this).repeat(times));
+  });
+  Handlebars.registerHelper("markdown", function (text: string) {
+    return new Handlebars.SafeString(marked.parse(text));
+  });
+  Handlebars.registerHelper("add", function (a: number, b: number) {
+    return a + b;
+  });
+  Handlebars.registerHelper("subtract", function (a: number, b: number) {
+    return a - b;
+  });
+  Handlebars.registerHelper(
+    "toLocaleDate",
+    function (locale: Intl.LocalesArgument, text: string) {
+      const date = new Date(text);
+      return date.toLocaleDateString(locale, {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+      });
+    }
+  );
+  const photoLocation = file
+    .replace(".en.yaml", ".jpg")
+    .replace(".yaml", ".jpg");
+  const hasPhoto = existsSync(`./data/${photoLocation}`);
+  const html = isEnglish
+    ? englishTemplate({
+        ...yaml,
+        file: hasPhoto ? photoLocation : null,
+      })
+    : template({
+        ...yaml,
+        file: hasPhoto ? photoLocation : null,
+      });
 
-  const hasPhoto = existsSync(`./data/${file.replace('.yaml', '.jpg')}`)
-  const html = template({ ...yaml, file: hasPhoto ? file.replace('.yaml', '.jpg') : null })
-
-  writeFileSync(`./output/${file.replace('.yaml', '.html')}`, html)
-})
+  writeFileSync(`./output/${file.replace(".yaml", ".html")}`, html);
+});
